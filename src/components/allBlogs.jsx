37da34535977b2bb898411c3dddf10027bd2db3a
newby101 <ishimwekevin45@gmail.com>
2023-02-18 21:29:42 +0200

    fixing routes adding blogs to landing page

--- src/components/allBlogs.jsx
+++ src/components/allBlogs.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import { NavLink } from "react-router-dom";
 
 function AllBlogs({num}) {
   const [blogs, setblogs] = useState([]);
@@ -26,9 +27,13 @@ function AllBlogs({num}) {
           return (
             <div className="flex justify-center mx-3 scale-95" key={_id}>
               <div className="rounded-lg shadow-2xl bg-white dark:bg-slate-900  max-w-sm">
-                <a href={`/my-brand-react-app/singleblog?id=${_id}`}>
+                <NavLink
+                  to={`/my-brand-react-app/singleblog?id=${_id}`}
+                  spy={true}
+                  smooth={true}
+                >
                   <img className="rounded-t-lg" src={blogImg} alt="" />
-                </a>
+                </NavLink>
                 <div className="p-6">
                   <h5 className="text-gray-900 text-xl dark:text-yellow-300 font-semibold ">
                     {blogTitle}
@@ -36,8 +41,10 @@ function AllBlogs({num}) {
                   <p className="text-gray-700 dark:text-gray-500 text-base h-24 overflow-hidden text-ellipsis whitespace-pre-wrap">
                     {blogDescription}
                   </p>
-                  <a
-                    href={`/my-brand-react-app/singleblog?id=${_id}`}
+                  <NavLink
+                    to={`/my-brand-react-app/singleblog?id=${_id}`}
+                    spy={true}
+                    smooth={true}
                     className=" decoration-none w-fit  flex px-2 py-1.5 dark:bg-[#182449] bg-yellow-500  mt-2 dark:text-yellow-400 text-black font-semibold text-sm  uppercase  shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   >
                     READ MORE
@@ -51,7 +58,7 @@ function AllBlogs({num}) {
                     >
                       <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
                     </svg>
-                  </a>
+                  </NavLink>
                 </div>
               </div>
             </div>
